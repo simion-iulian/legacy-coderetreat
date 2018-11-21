b5f4891701bb0f23e492122066ad1d193cebd75f
Gabriele Cipriano <>
2018-11-21 15:38:47 +0000

    improving readability

--- java/src/main/java/com/adaptionsoft/games/uglytrivia/QuestionDeck.java
+++ java/src/main/java/com/adaptionsoft/games/uglytrivia/QuestionDeck.java
@@ -1,46 +1,51 @@
 package com.adaptionsoft.games.uglytrivia;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.function.Predicate;
 import java.util.stream.IntStream;
 
 import static java.lang.String.*;
 import static java.util.Arrays.asList;
 
 public class QuestionDeck {
-    private final Category science;
-    private final Category sports;
-    private final Category rock;
-    private final Category pop;
     private final List<Category> categories;
 
     public QuestionDeck() {
-        pop = new Category("Pop", asList(0, 4, 8));
-        science = new Category("Science", asList(1, 5, 9));
-        sports = new Category("Sports", asList(2, 6, 10));
-        rock = new Category("Rock", asList(3, 7, 11));
-        categories = asList(pop, science, sports, rock);
+        categories = asList(
+                new Category("Pop", asList(0, 4, 8)),
+                new Category("Science", asList(1, 5, 9)),
+                new Category("Sports", asList(2, 6, 10)),
+                new Category("Rock", asList(3, 7, 11)));
     }
 
     String currentCategoryForPosition(int position) {
-        return categories
-                .stream()
-                .filter(category -> category.isPlacedOn(position))
-                .findFirst()
-                .orElseThrow(OutOfTheBoard::new)
-                .name();
+        return categoryFor(position).name();
     }
 
     String askQuestionFor(String categoryName) {
-        return categories
-                .stream()
-                .filter(category -> category.isNamed(categoryName))
-                .findFirst()
-                .orElseThrow(NoSuchCategory::new)
-                .nextQuestion();
+        return categoryFor(categoryName).nextQuestion();
     }
 
     public void fillQuestion() {
         IntStream.range(0, 50).forEach(i ->
                 categories.forEach(category -> category.addQuestion(format("%s Question %d", category.name(), i))));
+    }
+
+    private Optional<Category> findFirst(Predicate<Category> predicate) {
+        return categories
+                .stream()
+                .filter(predicate)
+                .findFirst();
+    }
+
+    private Category categoryFor(String categoryName) {
+        return findFirst(category -> category.isNamed(categoryName))
+                .orElseThrow(NoSuchCategory::new);
+    }
+
+    private Category categoryFor(int position) {
+        return findFirst(category -> category.isPlacedOn(position))
+                .orElseThrow(OutOfTheBoard::new);
     }
 }
