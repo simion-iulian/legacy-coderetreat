13652cfd0747b616593ed135222a08ff773eb207
Gabriele Cipriano <>
2018-11-21 11:25:01 +0000

    rock is placed in 3, 7,11

--- java/src/test/java/com/adaptionsoft/games/uglytrivia/QuestionDeckTest.java
+++ java/src/test/java/com/adaptionsoft/games/uglytrivia/QuestionDeckTest.java
@@ -13,6 +13,7 @@ class QuestionDeckTest {
             "0, Pop", "4, Pop", "8, Pop",
             "1, Science", "5, Science", "9, Science",
             "2, Sports", "6, Sports", "10, Sports",
+            "3, Rock", "7, Rock", "11, Rock",
     })
     void categories_placed_explicitly(int position, String category) {
         String actual = new QuestionDeck().currentCategoryForPosition(position);
