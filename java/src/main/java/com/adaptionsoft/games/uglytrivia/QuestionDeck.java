bba6b781c19f5df3a5ce7415c94a2171d57a00cf
Gabriele Cipriano <>
2018-11-21 10:34:01 +0000

    moved currentcategory method

--- java/src/main/java/com/adaptionsoft/games/uglytrivia/QuestionDeck.java
+++ java/src/main/java/com/adaptionsoft/games/uglytrivia/QuestionDeck.java
@@ -1,4 +1,16 @@
 package com.adaptionsoft.games.uglytrivia;
 
 public class QuestionDeck {
+    String currentCategoryForPosition(int position) {
+        if (position == 0) return "Pop";
+        if (position == 4) return "Pop";
+        if (position == 8) return "Pop";
+        if (position == 1) return "Science";
+        if (position == 5) return "Science";
+        if (position == 9) return "Science";
+        if (position == 2) return "Sports";
+        if (position == 6) return "Sports";
+        if (position == 10) return "Sports";
+        return "Rock";
+    }
 }
