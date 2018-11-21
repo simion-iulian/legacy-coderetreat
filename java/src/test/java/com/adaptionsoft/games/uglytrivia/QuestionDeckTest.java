a66d9270e39cfbb66e81d2c23caa861420cc056c
Gabriele Cipriano <>
2018-11-21 10:54:25 +0000

    testing pop category is placed at position 0

--- java/src/test/java/com/adaptionsoft/games/uglytrivia/QuestionDeckTest.java
+++ java/src/test/java/com/adaptionsoft/games/uglytrivia/QuestionDeckTest.java
@@ -1,7 +1,16 @@
 package com.adaptionsoft.games.uglytrivia;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 
 class QuestionDeckTest {
 
+    @Test
+    void pop_category() {
+        String actual = new QuestionDeck().currentCategoryForPosition(0);
+
+        assertThat(actual, is("Pop"));
+    }
 }
