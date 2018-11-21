d1ec385b5191e85f6bbcebd1c78f5174610f7e18
Gabriele Cipriano <>
2018-11-21 14:17:47 +0000

    Test class for category

--- java/src/test/java/com/adaptionsoft/games/uglytrivia/CategoryTest.java
+++ java/src/test/java/com/adaptionsoft/games/uglytrivia/CategoryTest.java
@@ -1,5 +1,13 @@
+package com.adaptionsoft.games.uglytrivia;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CategoryTest {
 
+    @Test
+    void is_placed() {
+
+    }
 }
