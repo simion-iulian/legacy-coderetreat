9cb965e4588df64d6f3fbeec377feae7556ac95f
Gabriele Cipriano <>
2018-11-21 11:04:35 +0000

    testing science and sports

--- java/src/test/java/com/adaptionsoft/games/uglytrivia/QuestionDeckTest.java
+++ java/src/test/java/com/adaptionsoft/games/uglytrivia/QuestionDeckTest.java
@@ -9,8 +9,12 @@ import static org.hamcrest.Matchers.is;
 class QuestionDeckTest {
 
     @ParameterizedTest
-    @CsvSource({"0, Pop", "4, Pop", "8, Pop"})
-    void pop_category(int position, String category) {
+    @CsvSource({
+            "0, Pop", "4, Pop", "8, Pop",
+            "1, Science", "5, Science", "9, Science",
+            "2, Sports", "6, Sports", "10, Sports",
+    })
+    void categories_placed_explicitly(int position, String category) {
         String actual = new QuestionDeck().currentCategoryForPosition(position);
 
         assertThat(actual, is(category));
