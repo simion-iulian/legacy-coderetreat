c0cbc39d2d3ae09251981e75e465b08d7b50cb1f
Gabriele Cipriano <>
2018-11-21 15:29:00 +0000

    using the list of categories for currentCategory method

--- java/src/main/java/com/adaptionsoft/games/uglytrivia/QuestionDeck.java
+++ java/src/main/java/com/adaptionsoft/games/uglytrivia/QuestionDeck.java
@@ -1,5 +1,7 @@
 package com.adaptionsoft.games.uglytrivia;
 
+import java.util.List;
+
 import static java.util.Arrays.asList;
 
 public class QuestionDeck {
@@ -7,21 +9,23 @@ public class QuestionDeck {
     private final Category sports;
     private final Category rock;
     private final Category pop;
+    private final List<Category> categories;
 
     public QuestionDeck() {
         pop = new Category("Pop", asList(0, 4, 8));
         science = new Category("Science", asList(1, 5, 9));
         sports = new Category("Sports", asList(2, 6, 10));
         rock = new Category("Rock", asList(3, 7, 11));
+        categories = asList(pop, science, sports, rock);
     }
 
     String currentCategoryForPosition(int position) {
-        if (pop.isPlacedOn(position)) return pop.name();
-        if (science.isPlacedOn(position)) return science.name();
-        if (sports.isPlacedOn(position)) return sports.name();
-        if (rock.isPlacedOn(position)) return rock.name();
-
-        throw new OutOfTheBoard();
+        return categories
+                .stream()
+                .filter(category -> category.isPlacedOn(position))
+                .findFirst()
+                .orElseThrow(OutOfTheBoard::new)
+                .name();
     }
 
     String askQuestionFor(String category) {
